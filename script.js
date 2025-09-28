be79761fa1aad77071dd361e851ca39156492189
Kaamibroken <kaamibroken64@gmail.com>
2025-09-29 03:19:58 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 async function getOtp() {
+  document.getElementById("output").textContent = "Loading OTP...";
   const res = await fetch("/api/otp");
   const data = await res.json();
   document.getElementById("output").textContent = JSON.stringify(data, null, 2);
 }
 
 async function getNumber() {
+  document.getElementById("output").textContent = "Loading Number...";
   const res = await fetch("/api/number");
   const data = await res.json();
   document.getElementById("output").textContent = JSON.stringify(data, null, 2);
 }
 
 async function getCountries() {
+  document.getElementById("output").textContent = "Loading Countries...";
   const res = await fetch("/api/countries");
   const data = await res.json();
   document.getElementById("output").textContent = JSON.stringify(data, null, 2);
 }
 
 async function getCarriers() {
+  document.getElementById("output").textContent = "Loading Carriers...";
   const res = await fetch("/api/carriers");
   const data = await res.json();
   document.getElementById("output").textContent = JSON.stringify(data, null, 2);
